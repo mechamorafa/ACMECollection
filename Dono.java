4b18b29bea296f5c19ba176efc6fefd73dbfa3f9
Rafael Ramos <rafaelramos@live.com>
2021-07-19 09:31:05 -0300

    Create Dono.java

---
+++ Dono.java
@@ -0,0 +1,20 @@
+public class Dono {
+
+	private String nome;
+	private String email;
+
+	public Dono(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+}
